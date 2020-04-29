b81d0c8a0782f40c67177278602d2d945bf6afca
Timo Weiser <timo.weiser@codify.ch>
2020-04-29 13:42:56 +0200

    initial commit

---
+++ src/main/java/de/bbqb/backend/api/controller/ApiController.java
@@ -0,0 +1,13 @@
+package de.bbqb.backend.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+  @GetMapping
+  public String test() {
+    return "App up and running";
+  }
+}

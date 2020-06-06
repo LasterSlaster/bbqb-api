ff78dc5993461cfd8e10ae6f5e2b7839e60b2d3d
Marius <lasterslaster@gmx.de>
2020-06-06 17:20:44 +0200

    Communicate with firestore via service instance

--- src/main/java/de/bbqb/backend/api/ApiApplication.java
+++ src/main/java/de/bbqb/backend/api/ApiApplication.java
@@ -3,6 +3,7 @@ package de.bbqb.backend.api;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gcp.data.firestore.repository.config.EnableReactiveFirestoreRepositories;
@@ -17,15 +18,25 @@ import org.springframework.integration.channel.DirectChannel;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
 
+import de.bbqb.backend.gcp.firestore.DeviceRepo;
+import de.bbqb.backend.gcp.firestore.document.DeviceDoc;
+
 @SpringBootApplication
 @EnableReactiveFirestoreRepositories("de.bbqb.backend.gcp.firestore")
 public class ApiApplication {
 	
 	private static final Log LOGGER = LogFactory.getLog(ApiApplication.class);
+	
+	private DeviceRepo deviceRepo;
+
+	@Value("${bbqb.gcp.pubsub.name}")
+	private String pubSubName;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
+	
 
 	/**
 	 * Provides an Adapter which listens to a GCP Pub/Sub subscription
@@ -38,12 +49,13 @@ public class ApiApplication {
 	  @Qualifier("pubsubInputChannel") MessageChannel inputChannel,
 	  PubSubTemplate pubSubTemplate) {
 		PubSubInboundChannelAdapter adapter =
-			  new PubSubInboundChannelAdapter(pubSubTemplate, "butler_info");
+			  new PubSubInboundChannelAdapter(pubSubTemplate, pubSubName); 
 		adapter.setOutputChannel(inputChannel);
 		adapter.setAckMode(AckMode.MANUAL);
 		
 		return adapter;
 	}
+
 
 	/**
 	 * Provides a channel to which a ChannelAdapter sends received messages 
@@ -54,6 +66,7 @@ public class ApiApplication {
 	  return new DirectChannel();
 	}
 
+
 	/**
 	 * Processes incoming messages from an InputChannel
 	 * @return
@@ -63,9 +76,25 @@ public class ApiApplication {
 	public MessageHandler messageReceiver() {
 		return message -> {
 			LOGGER.info("Message arrived! Payload: " + new String((byte[]) message.getPayload()));
+			//TODO: Write message to a time series database
+			// Retrieve attributes and message body from message
+			String deviceId = message.getHeaders().get("deviceId", String.class); //TODO: Move message attribute key to properties file
+			String deviceStatus = message.getPayload().toString();
+			Long deviceTimestamp = message.getHeaders().getTimestamp();
+
+			// Update device object
+			DeviceDoc deviceDoc = deviceRepo.findById(deviceId).block();
+			deviceDoc.setPublishTime(String.valueOf(deviceTimestamp));
+			deviceDoc.setStatus(deviceStatus);
+
+			// Update device document in database
+			deviceRepo.save(deviceDoc);
+			
+			// Acknowledge the message
 			BasicAcknowledgeablePubsubMessage originalMessage =
 			  message.getHeaders().get(GcpPubSubHeaders.ORIGINAL_MESSAGE, BasicAcknowledgeablePubsubMessage.class);
 			originalMessage.ack();
 		};
 	}
+
 }
--- src/main/java/de/bbqb/backend/api/controller/ApiController.java
+++ src/main/java/de/bbqb/backend/api/controller/ApiController.java
@@ -3,47 +3,61 @@ package de.bbqb.backend.api.controller;
 import java.net.URI;
 import java.util.stream.Stream;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import de.bbqb.backend.gcp.firestore.DeviceRepo;
-import de.bbqb.backend.gcp.firestore.document.DeviceDoc;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import de.bbqb.backend.api.model.entity.Device;
+import de.bbqb.backend.api.service.FirestoreDeviceService;
+import de.bbqb.backend.gcp.firestore.document.DeviceDoc; // TODO: Eliminate this dependency to database type
 
 @RestController
 public class ApiController {
 	
-	
-	private final DeviceRepo deviceRepo;
+	@Autowired
+	private FirestoreDeviceService deviceService;
 
-	public ApiController(DeviceRepo deviceRepo) {
-		this.deviceRepo = deviceRepo;
-	}
 
-	@GetMapping
-	public String test() {
-		return "App up and running";
-	}
-	
+	// Test endpoint only for development purposes
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello World";
 	}
-	 
-	@GetMapping("/devices")
-	public Stream<DeviceDoc> getDevices() {
-		Flux<DeviceDoc> devices = deviceRepo.findAll();
-		return devices.toStream();
-	}
 	
+
+	// TODO: Implement message publishing to communicate with iot devices
+	@PutMapping("/devices")
+	public ResponseEntity<Device> putDevices(@RequestBody Device device) { //TODO: Change return value
+
+		Device savedDevice = deviceService.updateDevice(device);
+		
+		// create Response
+		if (savedDevice == null) {
+	        return ResponseEntity.notFound().build();
+	    } else {
+	        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+	          .path("/{id}")
+	          .buildAndExpand(savedDevice.getId())
+	          .toUri();
+	 
+	        return ResponseEntity.created(uri)
+	         .body(savedDevice);
+	    }
+	}
+	 
+
 	@PostMapping("/devices")
-	public ResponseEntity<DeviceDoc> postDevices(@RequestBody DeviceDoc deviceDoc) {
-		DeviceDoc savedDevice = deviceRepo.save(deviceDoc).block();
+	public ResponseEntity<Device> postDevices(@RequestBody Device device) { //TODO: Change DeviceDoc to Device type 
+
+		Device savedDevice = deviceService.createDevice(device);
+		
+		// create Response
 		if (savedDevice == null) {
 	        return ResponseEntity.notFound().build();
 	    } else {
@@ -56,4 +70,32 @@ public class ApiController {
 	          .body(savedDevice);
 	    }
 	}
+
+
+	@GetMapping("/devices")
+	public Stream<DeviceDoc> getDevices() {
+
+		return deviceService.readAllDevices();
+	}
+	
+
+	@GetMapping("/devices/{deviceId}")
+	public ResponseEntity<Device> getDevice(@PathVariable("deviceId") String deviceId) {
+
+		Device device = deviceService.readDevice(deviceId);
+
+		// create Response
+		if (device == null) {
+	        return ResponseEntity.notFound().build();
+	    } else {
+	        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+	          .path("/{id}")
+	          .buildAndExpand(device.getId())
+	          .toUri();
+	 
+	        return ResponseEntity.created(uri)
+	          .body(device);
+	    }
+	}
+	
 }

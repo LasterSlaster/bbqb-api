c5f29009dc64bd20f5573bd878dee10cbb495630
Laster <lasterslaster@gmx.de>
2020-06-06 17:24:18 +0200

    Impl. publish message to pub/sub

--- src/main/java/de/bbqb/backend/api/ApiApplication.java
+++ src/main/java/de/bbqb/backend/api/ApiApplication.java
@@ -2,6 +2,7 @@ package de.bbqb.backend.api;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,9 +10,11 @@ import org.springframework.cloud.gcp.data.firestore.repository.config.EnableReac
 import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
 import org.springframework.cloud.gcp.pubsub.integration.AckMode;
 import org.springframework.cloud.gcp.pubsub.integration.inbound.PubSubInboundChannelAdapter;
+import org.springframework.cloud.gcp.pubsub.integration.outbound.PubSubMessageHandler;
 import org.springframework.cloud.gcp.pubsub.support.BasicAcknowledgeablePubsubMessage;
 import org.springframework.cloud.gcp.pubsub.support.GcpPubSubHeaders;
 import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.messaging.MessageChannel;
@@ -27,45 +30,58 @@ public class ApiApplication {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+	@Autowired
+	private PubsubOutboundGateway messagingGateway;
+
+	@Bean
+	@ServiceActivator(inputChannel = "pubsubOutputChannel")
+		public MessageHandler messageSender(PubSubTemplate pubsubTemplate) {
+		return new PubSubMessageHandler(pubsubTemplate, "testTopic"); // TODO: Change topic!
+	}
+
+	@MessagingGateway(defaultRequestChannel = "pubsubOutputChannel")
+	public interface PubsubOutboundGateway {
+		void sendToPubsub(String text);
+	}
 	/**
 	 * Provides an Adapter which listens to a GCP Pub/Sub subscription
 	 * @param inputChannel
 	 * @param pubSubTemplate
 	 * @return
 	 */
-	@Bean
-	public PubSubInboundChannelAdapter messageChannelAdapter(
-	  @Qualifier("pubsubInputChannel") MessageChannel inputChannel,
-	  PubSubTemplate pubSubTemplate) {
-		PubSubInboundChannelAdapter adapter =
-			  new PubSubInboundChannelAdapter(pubSubTemplate, "butler_info");
-		adapter.setOutputChannel(inputChannel);
-		adapter.setAckMode(AckMode.MANUAL);
-		
-		return adapter;
-	}
-
-	/**
-	 * Provides a channel to which a ChannelAdapter sends received messages 
-	 * @return
-	 */
-	@Bean
-	public MessageChannel pubsubInputChannel() {
-	  return new DirectChannel();
-	}
-
-	/**
-	 * Processes incoming messages from an InputChannel
-	 * @return
-	 */
-	@Bean
-	@ServiceActivator(inputChannel = "pubsubInputChannel")
-	public MessageHandler messageReceiver() {
-		return message -> {
-			LOGGER.info("Message arrived! Payload: " + new String((byte[]) message.getPayload()));
-			BasicAcknowledgeablePubsubMessage originalMessage =
-			  message.getHeaders().get(GcpPubSubHeaders.ORIGINAL_MESSAGE, BasicAcknowledgeablePubsubMessage.class);
-			originalMessage.ack();
-		};
-	}
+	//@Bean
+	//public PubSubInboundChannelAdapter messageChannelAdapter(
+	  //@Qualifier("pubsubInputChannel") MessageChannel inputChannel,
+	  //PubSubTemplate pubSubTemplate) {
+		//PubSubInboundChannelAdapter adapter =
+			  //new PubSubInboundChannelAdapter(pubSubTemplate, "butler_info");
+		//adapter.setOutputChannel(inputChannel);
+		//adapter.setAckMode(AckMode.MANUAL);
+		//
+		//return adapter;
+	//}
+//
+	///**
+	 //* Provides a channel to which a ChannelAdapter sends received messages 
+	 //* @return
+	 //*/
+	//@Bean
+	//public MessageChannel pubsubInputChannel() {
+	  //return new DirectChannel();
+	//}
+//
+	///**
+	 //* Processes incoming messages from an InputChannel
+	 //* @return
+	 //*/
+	//@Bean
+	//@ServiceActivator(inputChannel = "pubsubInputChannel")
+	//public MessageHandler messageReceiver() {
+		//return message -> {
+			//LOGGER.info("Message arrived! Payload: " + new String((byte[]) message.getPayload()));
+			//BasicAcknowledgeablePubsubMessage originalMessage =
+			  //message.getHeaders().get(GcpPubSubHeaders.ORIGINAL_MESSAGE, BasicAcknowledgeablePubsubMessage.class);
+			//originalMessage.ack();
+		//};
+	//} 
 }
--- src/main/java/de/bbqb/backend/api/controller/ApiController.java
+++ src/main/java/de/bbqb/backend/api/controller/ApiController.java
@@ -3,13 +3,16 @@ package de.bbqb.backend.api.controller;
 import java.net.URI;
 import java.util.stream.Stream;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import de.bbqb.backend.api.ApiApplication.PubsubOutboundGateway;
 import de.bbqb.backend.gcp.firestore.DeviceRepo;
 import de.bbqb.backend.gcp.firestore.document.DeviceDoc;
 import reactor.core.publisher.Flux;
@@ -20,6 +23,9 @@ public class ApiController {
 	
 	
 	private final DeviceRepo deviceRepo;
+
+	@Autowired
+	private PubsubOutboundGateway messagingGateway;
 
 	public ApiController(DeviceRepo deviceRepo) {
 		this.deviceRepo = deviceRepo;
@@ -39,6 +45,29 @@ public class ApiController {
 	public Stream<DeviceDoc> getDevices() {
 		Flux<DeviceDoc> devices = deviceRepo.findAll();
 		return devices.toStream();
+	}
+	
+	/**
+	 * publish a message to a device like open bbqb
+	 * @param deviceDoc
+	 * @return
+	 */
+	@PutMapping("/devices") 
+	public ResponseEntity<DeviceDoc> putDevices(@RequestBody DeviceDoc deviceDoc) {
+		messagingGateway.sendToPubsub("message"); // TODO: Update message payload
+
+		DeviceDoc savedDevice = null; // TODO: updat this part
+		if (savedDevice == null) {
+	        return ResponseEntity.notFound().build();
+	    } else {
+	        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+	          .path("/{id}")
+	          .buildAndExpand(savedDevice.getId())
+	          .toUri();
+	 
+	        return ResponseEntity.created(uri)
+	          .body(savedDevice);
+	    }
 	}
 	
 	@PostMapping("/devices")

71457f4799b6885fb8190ef020aed5e1f6b3813d
Laster <lasterslaster@gmx.de>
2020-06-18 17:10:31 +0200

    read message header from prop file

--- src/main/java/de/bbqb/backend/gcp/firestore/DeviceMessageHandler.java
+++ src/main/java/de/bbqb/backend/gcp/firestore/DeviceMessageHandler.java
@@ -2,6 +2,7 @@ package de.bbqb.backend.gcp.firestore;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gcp.pubsub.support.BasicAcknowledgeablePubsubMessage;
 import org.springframework.cloud.gcp.pubsub.support.GcpPubSubHeaders;
 import org.springframework.messaging.Message;
@@ -13,6 +14,12 @@ import de.bbqb.backend.gcp.firestore.document.DeviceDoc;
 public class DeviceMessageHandler implements MessageHandler {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(DeviceMessageHandler.class);
+	
+	/**
+	 * Name of the gcp pub/sub topic where bbqb devices send messages to
+	 */
+	@Value("${bbq.backend.gcp.pubsub.message-header-deviceid}")
+	private String deviceIdMessageHeader;
 
 	/**
 	 * An Interface to write and read device information from/to gcp nosql firestore
@@ -22,9 +29,8 @@ public class DeviceMessageHandler implements MessageHandler {
 	@Override
 	public void handleMessage(Message<?> message) throws MessagingException {
 		LOGGER.info("Message arrived! Payload: " + new String((byte[]) message.getPayload()));
-			//TODO: Write message to a time series database
 			// Retrieve attributes and message body from message
-			String deviceId = message.getHeaders().get("deviceId", String.class); //TODO: Move message attribute key to properties file
+			String deviceId = message.getHeaders().get(deviceIdMessageHeader, String.class);
 			String deviceStatus = message.getPayload().toString();
 			Long deviceTimestamp = message.getHeaders().getTimestamp();
 

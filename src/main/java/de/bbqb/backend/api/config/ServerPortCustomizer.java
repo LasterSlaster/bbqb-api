ff20af2b68b28776265e8753cf73c50f52e0c0c7
Laster <lasterslaster@gmx.de>
2020-06-20 02:04:25 +0200

    read portnumber from env

---
+++ src/main/java/de/bbqb/backend/api/config/ServerPortCustomizer.java
@@ -0,0 +1,36 @@
+package de.bbqb.backend.api.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServerPortCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    @Autowired
+    private Environment env;		  
+    
+    @Value("bbq.backend.webserver.port-env-variable")
+    private String WebserverPortEnvVariable;
+    
+	private static final Logger LOGGER = LoggerFactory.getLogger(ServerPortCustomizer.class);
+
+	@Override
+    public void customize(ConfigurableWebServerFactory factory) {
+		String portValue = env.getProperty(WebserverPortEnvVariable);
+		int port = 8080;
+		try {
+			port = Integer.parseInt(portValue);
+			LOGGER.info("Configure webserver port with port number " + String.valueOf(port) + " specified by environment variable " + WebserverPortEnvVariable);
+		} catch (NumberFormatException e) {
+			LOGGER.info("Unable to resolve a valid port number from environment variable " + WebserverPortEnvVariable + ". Fallback to default port 8080.");
+		}
+		
+        factory.setPort(port);
+    }
+}

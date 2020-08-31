a34ed1772457fd6c6d96a2bc7648bb7eda4e2011
Marius <lasterslaster@gmx.de>
2020-08-31 19:47:51 +0200

    Permit all requests

--- src/main/java/de/bbqb/backend/api/config/AppSecurityConfig.java
+++ src/main/java/de/bbqb/backend/api/config/AppSecurityConfig.java
@@ -1,17 +1,19 @@
 package de.bbqb.backend.api.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
+@Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(authorize -> authorize
-                        .anyRequest().authenticated()
-                ).oauth2Login(withDefaults());
+                        //.anyRequest().authenticated()
+                        .anyRequest().permitAll()
+                );
     }
 }

e1afeccff88165424760aa6712081aa2eb5a566f
Marius <lasterslaster@gmx.de>
2020-09-03 15:18:39 +0200

    Enable spring boot test

--- src/test/java/de/bbqb/backend/api/ApiApplicationTests.java
+++ src/test/java/de/bbqb/backend/api/ApiApplicationTests.java
@@ -1,8 +1,9 @@
 package de.bbqb.backend.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class ApiApplicationTests {
 
     @Test

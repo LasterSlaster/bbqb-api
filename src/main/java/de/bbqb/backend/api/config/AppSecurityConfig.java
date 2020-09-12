5ec5b4787e0cc5ba0dc5a20aae466d0275bb1331
Marius Degen <lasterslaster@gmx.de>
2020-09-12 03:09:33 +0200

    Permit all requests to start endpoint

--- src/main/java/de/bbqb/backend/api/config/AppSecurityConfig.java
+++ src/main/java/de/bbqb/backend/api/config/AppSecurityConfig.java
@@ -29,10 +29,12 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .mvcMatchers(HttpMethod.GET,"/").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/devices").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/_ah/start").permitAll() // Required by GAE to start up an instance
                 .anyRequest().authenticated()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
+    // TODO: Configure CORS appropriately
     //@Bean
     //CorsConfigurationSource corsConfigurationSource() {
     //    CorsConfiguration configuration = new CorsConfiguration();

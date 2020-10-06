dd3f58ddd28195c31a09a2d56bc28e81331099fc
Marius Degen <lasterslaster@gmx.de>
2020-10-06 23:48:42 +0200

    Permit all user endpoints for dev

--- src/main/java/de/bbqb/backend/api/config/AppSecurityConfig.java
+++ src/main/java/de/bbqb/backend/api/config/AppSecurityConfig.java
@@ -29,6 +29,10 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .mvcMatchers(HttpMethod.GET,"/").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/users").permitAll() // TODO: only an authenticated client should be able to read its users information
+                .mvcMatchers(HttpMethod.GET, "/users/*").permitAll() // TODO: only some sort of admin should be able to read all users information
+                .mvcMatchers(HttpMethod.POST, "/users").permitAll() // TODO: Rethink this endpoint. The client should not be the one who connects our identity servers information with our api-service
+                .mvcMatchers(HttpMethod.PUT, "/users/*").permitAll() // TODO: only an authenticated client should be able to read its users information
                 .mvcMatchers(HttpMethod.GET, "/devices").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/devices/*").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/_ah/start").permitAll() // Required by GAE to start up an instance

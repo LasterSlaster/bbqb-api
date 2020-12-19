31fdaf66d4953a47e23c3ccad792a78adaac5f2d
Marius Degen <lasterslaster@gmx.de>
2020-12-19 17:14:17 +0100

    Add missing booking constructor

--- src/main/java/de/bbqb/backend/api/model/entity/Booking.java
+++ src/main/java/de/bbqb/backend/api/model/entity/Booking.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 import java.util.Date;
 
 @Getter
-@AllArgsConstructor
 public class Booking {
+    public Booking() {}
     private String id;
     private String paymentIntentId; // TODO: Remove this field. Redundant, also in payment as id
     private String deviceId;
@@ -17,4 +17,53 @@ public class Booking {
     private Date sessionStart;
     private Payment payment;
     private Integer timeslot;
+
+
+    public Booking(String id, String paymentIntentId, String deviceId, String userId, String status, Date requestTime, Date sessionStart, Payment payment, Integer timeslot) {
+        this.id = id;
+        this.paymentIntentId = paymentIntentId;
+        this.deviceId = deviceId;
+        this.userId = userId;
+        this.status = status;
+        this.requestTime = requestTime;
+        this.sessionStart = sessionStart;
+        this.payment = payment;
+        this.timeslot = timeslot;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPaymentIntentId() {
+        return paymentIntentId;
+    }
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Date getRequestTime() {
+        return requestTime;
+    }
+
+    public Date getSessionStart() {
+        return sessionStart;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public Integer getTimeslot() {
+        return timeslot;
+    }
 }

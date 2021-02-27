ffc791a72f5b5f5e70cbdbc1736074d3098ef76b
Marius Degen <lasterslaster@gmx.de>
2021-02-27 18:47:39 +0100

    Move files

--- src/main/java/de/bbqb/backend/api/model/service/CustomerService.java
+++ src/main/java/de/bbqb/backend/api/model/service/CustomerService.java
@@ -1,7 +1,8 @@
 package de.bbqb.backend.api.model.service;
 
 /**
- * Business logic to manage users
+ * Business logic to manage customers.
+ * Customers are used to make payments for BBQB services
  *
  * @author Marius Degen
  */

618a846c69272a2f187d8ea112ad1bdd81113049
Marius Degen <lasterslaster@gmx.de>
2021-02-27 18:56:30 +0100

    Update test docu

--- src/test/java/de/bbqb/backend/api/ApiApplicationTests.java
+++ src/test/java/de/bbqb/backend/api/ApiApplicationTests.java
@@ -4,11 +4,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
+ * Application integration tests
+ *
  * @author Marius Degen
  */
 @SpringBootTest
 class ApiApplicationTests {
 
+    /**
+     * Tests if bean creation and context initialization is successful.
+     */
     @Test
     void contextLoads() {
 
